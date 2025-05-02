e71bc701579ad61132037125a70f247aa9eb22e6
Khaled <khaloodytm4@gmail.com>
2025-05-02 15:14:21 +0300

    Update resume_reader.py
    Removed unnecessary else statement and job_skills_list=None to simplify the program.

--- src/resume_reader.py
+++ src/resume_reader.py
@@ -11,15 +11,13 @@ def extract_text_from_txt(file_path):
         print(f"Error reading file {file_path}: {e}")
         return ""
 
-def extract_skills(text, job_skills_list=None):
+def extract_skills(text, job_skills_list):
     try:
         if job_skills_list:
             all_skills = list(set(' '.join(job_skills_list).lower().split()))
             text_words = set(re.findall(r'\b\w[\w+.-]*\b', text.lower()))
             found_skills = [skill for skill in all_skills if skill in text_words]
-        else:
-            pattern = r'\b(Python|Java|C\+\+|Machine Learning|Data Analysis|Excel|Communication|Deep Learning)\b'
-            found_skills = re.findall(pattern, text, flags=re.IGNORECASE)
+        
         return list(set([skill.lower() for skill in found_skills]))
     except Exception as e:
         print(f"Error extracting skills: {e}")
