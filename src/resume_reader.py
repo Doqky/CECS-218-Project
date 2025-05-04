b5e040cf466a75efa1ab0dc891035e78e859ce1a
real_yazan <163666352+sdyzf@users.noreply.github.com>
2025-05-04 19:35:37 +0300

    Update resume_reader.py

--- src/resume_reader.py
+++ src/resume_reader.py
@@ -6,19 +6,13 @@ def extract_text_from_txt(file_path):
             return file.read()
     except FileNotFoundError:
         print(f"Error: File not found - {file_path}")
-        return ""
     except Exception as e:
         print(f"Error reading file {file_path}: {e}")
-        return ""
 
-def extract_skills(text, job_skills_list):
+def extract_skills( text , job_skills_list):
     try:
-        if job_skills_list:
-            all_skills = list(set(' '.join(job_skills_list).lower().split()))
-            text_words = set(re.findall(r'\b\w[\w+.-]*\b', text.lower()))
-            found_skills = [skill for skill in all_skills if skill in text_words]
-        
-        return list(set([skill.lower() for skill in found_skills]))
+        all_skills = list(set(' '.join(job_skills_list).lower().split()))
+        text_words = list(set(re.findall(r'\b\w[\w+.]*\b', text.lower())))
+        return [skill for skill in all_skills if skill in text_words]
     except Exception as e:
         print(f"Error extracting skills: {e}")
-        return []
