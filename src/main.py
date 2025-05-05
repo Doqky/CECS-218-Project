310501deeef7b5637d70ba566b774882fe147a54
Khaled <khaloodytm4@gmail.com>
2025-05-05 14:39:24 +0300

    Update main.py
    added comments

--- src/main.py
+++ src/main.py
@@ -3,18 +3,19 @@ from resume_reader import extract_text_from_txt, extract_skills
 from skill_matcher import evaluate_resumes
 
 try:
-    jobs_df = pd.read_csv('../Resume_Data/jobs.csv')
-    job_roles = jobs_df['role']
-    job_skills = jobs_df['skills'].tolist()
+    jobs_df = pd.read_csv('../Resume_Data/jobs.csv') # Read the CSV file containing job roles and skills in a DataFrame format 
+    job_roles = jobs_df['role'] # List of job roles in Series format
+    job_skills = jobs_df['skills'].tolist() # List of job skills in Series format
 
-    resume_files = [
+    resume_files = [ # List of resume file paths
         '../Resume_Data/resumes/resume_1.txt',
         '../Resume_Data/resumes/resume_2.txt',
         '../Resume_Data/resumes/resume_3.txt'
     ]
 
-    evaluate_resumes(resume_files, job_skills, extract_text_from_txt, extract_skills, job_roles)
+    evaluate_resumes(resume_files, job_skills, extract_text_from_txt, extract_skills, job_roles) # This function evaluates the resumes and returns the results
+
 except FileNotFoundError:
     print("Error: jobs.csv not found in Resume_Data directory")
 except Exception as e:
-    print(f"Unexpected error in main program: {e}")
+    print(f"Unexpected error in main program: {e}") 
