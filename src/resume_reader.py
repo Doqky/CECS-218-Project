d6168a2edca083845e050fb2495d4d05a1529e5d
Khaled <khaloodytm4@gmail.com>
2025-05-05 14:43:30 +0300

    Update resume_reader.py
    added comments and docstrings

--- src/resume_reader.py
+++ src/resume_reader.py
@@ -1,23 +1,47 @@
 import re
 
-def extract_text_from_txt(file_path):
+def extract_text_from_txt(file_path): 
+    """
+    Extracts text from a .txt file.
+
+    Args:
+        file_path (str): Path to the .txt file.
+
+    Returns:
+        str: Extracted text from the file.
+
+    """
+
     try:
-        with open(file_path, 'r') as file:
-            return file.read()
+        with open(file_path, 'r') as file: # Open the file in read mode
+            return file.read() # Read the content of the file and return it as a string
+        
     except FileNotFoundError:
         print(f"Error: File not found - {file_path}")
     except Exception as e:
         print(f"Error reading file {file_path}: {e}")
 
 def extract_skills( text , job_skills_list):
-    try:
-        all_skills = list(set(' '.join(job_skills_list).lower().split()))
-        text_words = list(set(re.findall(r'\b\w[\w+.]*\b', text.lower())))
-        matched_skills = []
+    """
+    Extracts skills from the given text based on a list of job skills.
 
-        for skill in all_skills:
+    Args:
+        text (str): The text from which to extract skills.
+        job_skills_list (list): A list of job skills.
+
+    Returns:
+            list: A list of matched skills found in the text.
+
+    """
+
+    try:
+        all_skills = list(set(' '.join(job_skills_list).lower().split())) #Converts the skills into a list of unique skills and lowercases them
+        text_words = list(set(re.findall(r'\b\w[\w+.]*\b', text.lower()))) #Converts the text into a list of unique words and lowercases them
+        matched_skills = [] # Initialize an empty list to store matched skills
+
+        for skill in all_skills: #Iterate through the list of skills
             if skill in text_words:
-                matched_skills.append(skill)
+                matched_skills.append(skill) #If the skill is found in the text, append it to the matched_skills list
         
         return matched_skills
 
